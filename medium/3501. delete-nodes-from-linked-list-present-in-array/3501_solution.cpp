9435a7afd4167d776a5b3489f89430d12b512288
Siddhant <sidcodeshere@gmail.com>
2024-09-06 11:04:34 +0530

    147 solved

---
+++ medium/3501. delete-nodes-from-linked-list-present-in-array/3501_solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> toRemove(nums.begin(), nums.end());
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* curr = dummy;
+
+        while (curr->next != NULL) {
+            if (toRemove.count(curr->next->val)) {
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
+            }
+        }
+
+        return dummy->next;
+    }
+};
