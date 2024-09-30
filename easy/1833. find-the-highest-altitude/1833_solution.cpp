e0f455f2d91787e4a73115dec657e89f1092170a
itssiddhant <sidcodeshere@gmail.com>
2024-09-30 19:13:03 +0530

    30 September

---
+++ easy/1833. find-the-highest-altitude/1833_solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maxAlt = 0;
+        int currAlt = 0;
+        for(int i =0; i<gain.size();i++){
+            currAlt+=gain[i];
+            maxAlt=max(maxAlt,currAlt);
+        }
+        return max(maxAlt,0);
+    }
+};
