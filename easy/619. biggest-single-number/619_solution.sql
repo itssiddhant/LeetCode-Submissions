b63e0adfa6525ccff044f684bc25483760e63f7f
itssiddhant <sidcodeshere@gmail.com>
2024-10-22 22:48:47 +0530

    22 October

---
+++ easy/619. biggest-single-number/619_solution.sql
@@ -0,0 +1,3 @@
+select max(num) as num from
+(select num from mynumbers group by num having count(num)=1)
+as unique_numbers;
---
+++ medium/1116. maximum-level-sum-of-a-binary-tree/1116_solution.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        int maxLevel = 1;
+        int maxSum = INT_MIN;
+        int level = 1;
+
+        while (!q.empty()) {
+            int levelSum = 0;
+            int levelSize = q.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                levelSum += node->val;
+
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+
+            if (levelSum > maxSum) {
+                maxSum = levelSum;
+                maxLevel = level;
+            }
+            level++;
+        }
+        return maxLevel;
+    }
+};
