7c877dd8a4cbfdc8f7e673fdd6180760736f59cf
itssiddhant <sidcodeshere@gmail.com>
2024-11-17 00:57:55 +0530

    16 November

---
+++ medium/62. unique-paths/62_solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> curr(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                curr[j] = curr[j] +curr[j - 1];
+            }
+        }
+        return curr[n - 1];
+    }
+};
