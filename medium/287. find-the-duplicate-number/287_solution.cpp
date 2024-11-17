9e4bce2ee8852e2d98c71c2ed923cd12ff18fb35
itssiddhant <sidcodeshere@gmail.com>
2024-11-17 23:03:19 +0530

    17 November

---
+++ medium/287. find-the-duplicate-number/287_solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for (int num : nums) {
+            int idx = abs(num);
+            if (nums[idx] < 0) {
+                return idx;
+            }
+            nums[idx] = -nums[idx];
+        }
+
+        return n;
+    }
+};
