27d0905b33599143febc9759456b9c80822448d5
itssiddhant <sidcodeshere@gmail.com>
2024-11-29 20:00:55 +0530

    29 November

---
+++ medium/80. remove-duplicates-from-sorted-array-ii/80_solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i =0;
+        for(int j:nums){
+            if (i<2 || j>nums[i-2]){
+                nums[i++]=j;
+            }
+        }
+        return i;
+    }
+};
